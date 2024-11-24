eca15195d3c1c806f90b58b0177131a3e1c4c819
Yash <yasharmag01@gmail.com>
2024-11-24 11:36:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from tkinter import Button, Tk
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
